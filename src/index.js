38eb6ab48a76789759aa749dcb4c3a6d23d60d10
Trung9h <fecoder.chinh@gmail.com>
2022-04-27 08:11:40 +0700

    save data

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ export default class SimpleCarousel {
    */
   constructor({ data, config, api, readOnly  }) {
     this.api = api;
-    this._data = {};
     this.data = data;
     this.IconClose = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon--cross" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>';
     this.IconLeft = '<svg xmlns="http://www.w3.org/2000/svg" class="icon " width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>';
@@ -315,7 +314,7 @@ export default class SimpleCarousel {
    */
   tuneToggled(tuneName) {
     // inverse tune state
-    this.setTune(tuneName, !this._data[tuneName]);
+    this.setTune(tuneName, !this.data[tuneName]);
   }
 
   /**
@@ -326,7 +325,7 @@ export default class SimpleCarousel {
    * @returns {void}
    */
   setTune(tuneName, value) {
-    this._data[tuneName] = value;
+    this.data[tuneName] = value;
 
     this.applyTune(tuneName, value);
 
@@ -362,7 +361,7 @@ export default class SimpleCarousel {
    * @returns {Element}
    */
   renderSettings() {
-    return this.tunes.render(this._data);
+    return this.tunes.render(this.data);
   }
 
   /**
